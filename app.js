c94fef0a18dcae783d49c8840af66647ee9d0daf
Alz3bi <mohammed.alzoubi.dev@gmail.com>
2024-03-29 16:57:33 +0000

    update app.js

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
-var UserName= prompt("please Enter your Name")
+'use strict';
 
-var UserGender= prompt("please Enter your Gender, Male/Female")
+const myTimeout = setTimeout(2000);
 
-var UserAge= prompt("please Enter your Age")
+var userName= prompt("please Enter your Name")
 
-if (UserAge <= 0) {
+var userGender= prompt("please Enter your Gender, Male/Female").toLowerCase()
+
+var userAge= prompt("please Enter your Age")
+
+if (userAge <= 0) {
     alert("age should not be less or equal to zero")
 }
 
-var Userprefix=""
+var userprefix=""
 
-if (UserGender == "Male") {
-    Userprefix = "Mr"
+if (userGender == "male") {
+    userprefix = "Mr"
 }
-else if(UserGender == "Female"){
-    Userprefix = "Ms"
+else if(userGender == "Female"){
+    userprefix = "Ms"
 }
 
 var confirmResult= confirm("Do you want to skip the welcoming message")
 
 if (confirmResult == false) {
-    alert("Welcome "+Userprefix+" "+UserName)
+    alert("Welcome "+userprefix+" "+userName)
 }
+
+const answersArray = []
+
+function askFunction(question){
+    let answer = prompt(question)
+    if(answer == ""){
+        answersArray.push("invalid")
+    }else {
+        answersArray.push(answer)
+    }
+}
+
+askFunction("1")
+askFunction("2")
+askFunction("3")
+
+answersArray.forEach((answer) => console.log(answer));
